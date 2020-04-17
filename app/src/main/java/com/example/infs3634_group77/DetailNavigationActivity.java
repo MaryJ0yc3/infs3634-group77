f2ebc298489a989035e37d1d8669300913a47149
MaryJ0yc3 <z5207526@ad.unsw.edu.au>
2020-04-17 12:18:44 +1000

    adding detail navigation activity (where the navigation bar is. blow up the fragments onto this activity)

---
+++ app/src/main/java/com/example/infs3634_group77/DetailNavigationActivity.java
@@ -0,0 +1,14 @@
+package com.example.infs3634_group77;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailNavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_navigation);
+    }
+}
