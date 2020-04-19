8454b253eccc7da64e466958315081846f20167c
ls-ch <61213028+ls-ch@users.noreply.github.com>
2020-04-19 22:18:20 +1000

    Made a nav bar, not 100% fixed

--- app/src/main/java/com/example/infs3634_group77/MainActivity.java
+++ app/src/main/java/com/example/infs3634_group77/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.infs3634_group77;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.infs3634_group77.LearningFightScore.LearningScreenFragment;
+import com.example.infs3634_group77.Settings.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +17,40 @@ public class MainActivity extends AppCompatActivity {
     //Just has 'start' button and nice graphic behind with logo.
     //can add progress bar?
     //Joyce might add animations later
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialising bottom navigation
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        //Navigation straight to home screen if previously haven't used app
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new HomeScreenFragment()).commit();
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        fragment = new HomeScreenFragment();
+                        break;
+                    case R.id.search:
+                        fragment = new LearningScreenFragment();
+                        break;
+                    case R.id.settings:
+                        fragment = new SettingsFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,fragment).commit();
+                return true;
+            }
+        });
     }
 }
