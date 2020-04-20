b0d3f1638e01209c9fb8aeafdb0b9f4b5081c4bc
ls-ch <61213028+ls-ch@users.noreply.github.com>
2020-04-21 00:16:15 +1000

    making recyclerview in fragment home

--- app/src/main/java/com/example/infs3634_group77/HomeScreenFragment.java
+++ app/src/main/java/com/example/infs3634_group77/HomeScreenFragment.java
@@ -2,16 +2,21 @@ package com.example.infs3634_group77;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.infs3634_group77.Helpers.CategoryAdapter;
+
+import java.util.ArrayList;
 
 
 public class HomeScreenFragment extends Fragment {
-
     //has category recycler view horizontal. if lazy just do buttons but
     // can get marked down coz it limits the cards we can add later
 
@@ -27,9 +32,18 @@ public class HomeScreenFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false);
+        View view = inflater.inflate(R.layout.home_row, container, false);
+        RecyclerView mRecyclerView = view.findViewById(R.id.rvHome);
+
+        CategoryAdapter mAdapter = new CategoryAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        return view;
     }
 }
